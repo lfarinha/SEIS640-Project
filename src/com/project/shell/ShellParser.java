c15b3affba3c40469b5e0ffe91bcc2f0205693cf
leonardo <leonardo@linux-ropq>
2016-03-10 21:29:25 -0600

    History is working now!

--- src/com/project/shell/ShellParser.java
+++ src/com/project/shell/ShellParser.java
@@ -4,40 +4,43 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-import org.omg.CORBA.portable.InputStream;
 
 public class ShellParser
 {
 	
-	private int lineCount=1;
-	private String commandLine, tag="LF, RD>";
+	private int lineCount=1, aCount=0;
+	private String commandLine, tag="LF,RD";
+	private String[] lineHistory = new String[100];
 
 	
 	public void readLine()
 	{
-		
-	//ParserHistory history;
-		
+			
 	try
 	{
 	InputStreamReader line = new InputStreamReader(System.in);
 	BufferedReader console = new BufferedReader(line);
 	
+	
 	// we break out with <control><C>
 	while (true) {
 	  // read what the user entered
-	  System.out.print(this.tag+this.lineCount+" ");
+	  System.out.print(this.tag+"-"+this.lineCount+"> ");
 	  commandLine = console.readLine();
+	  	  
+	  
+	  String[] shellHistory = commandLine.split(" ");
+
+	  
 	  	  
 	  // if the user entered a return, just loop again
 	  if (commandLine.equals(""))
 	  {
+	      lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
 		  this.lineCount++;
-		continue;
+		  aCount++;
+		  continue;
 		} else if(commandLine.equalsIgnoreCase("exit"))  
 		{
 		  this.lineCount++;
@@ -45,48 +48,53 @@ public class ShellParser
 		  System.exit(0);
 		} else if(commandLine.equalsIgnoreCase("version"))  
 		{
-	      this.lineCount++;
-		  System.out.println("current version: v1.0");
+			lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
+			  this.lineCount++;
+			  System.out.println("current version: v1.0");
+			  aCount++;
 		} else if(commandLine.equalsIgnoreCase("help"))  
 		{
+			  lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
 		      this.lineCount++;
-			  System.out.println("list of commands: exit, version, help, time, date");
+			  System.out.println("list of commands: exit, version, help, time, date, history");
+			  aCount++;
 		}
 		else if(commandLine.equalsIgnoreCase("time"))  
 		{
+			  lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
 			  String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
 		      this.lineCount++;
 			  System.out.println("The current time is: "+timeStamp.toString());
+			  aCount++;
 		}else if(commandLine.equalsIgnoreCase("date"))  
 		{
+			  lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
 			  Date date = new Date();
 			  System.out.println(date.toString());
 		      this.lineCount++;
-		}else if (commandLine.equals("exit")==false || commandLine.equals("version")==false  || commandLine.equals("time")==false || commandLine.equals("date")==false)
+			  aCount++;
+		}else if (commandLine.equalsIgnoreCase("history"))
 		{
+			lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
+  			for(int i=0; i<this.aCount; i++)
+  			{
+  			System.out.println(lineHistory[i]);
+  			}
+			this.lineCount++;
+			aCount++;
+		}else if (commandLine.equals("exit")==false || commandLine.equals("version")==false  || commandLine.equals("time")==false || commandLine.equals("date")==false || commandLine.equals("history")==false)
+		{
+			lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
 			System.out.println("I can't recognize that command, maybe you should try the help command ;)");
 			this.lineCount++;
-		}
-	  
-	  
-	  	ArrayList<String> readLines = new ArrayList<String>();
-	  	String[] shellHistory = commandLine.split(" ");
-
-	  	int size = shellHistory.length;
-	  	for (int i=0; i<size; i++)  {
-
-	  		readLines.add(this.tag+(this.lineCount-1)+" "+shellHistory[i]); 
-	  		System.out.println(readLines.get(i));  // testing, wanted to make sure its working
-	  		}
-	  			
-	  			
-	  
-
+			aCount++;
+		}	
 	 
-	  
 	  }
     } catch (Exception e) {
         e.printStackTrace();
       }
 	}
+	
+
 }
