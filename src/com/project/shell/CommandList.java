8e5c3009d5f13e50ca05bfbec9cc321ddba2bb84
leonardo <leonardo@linux-ropq>
2016-03-11 12:58:50 -0600

    CHANGED history implementation from Arrays to Arraylist.

--- src/com/project/shell/CommandList.java
+++ src/com/project/shell/CommandList.java
@@ -1,17 +1,76 @@
 package com.project.shell;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+
 public class CommandList {
 	
-	CommandList exit, version, help, time, date;
+	String EXIT="Goodbye", 
+		   VERSION="Leonardo Farinha & Rohan Dayananda's CLI, Version 1.0", 
+		   HELP="list of commands: exit, version, help, time, date, history",
+		   TIME="The current time is: ",
+		   DATE="";
 	
-	public Boolean checkCommand(String command)
+	public void checkCommand(StringStack stack, int aCount, int lineCount, String commandLine, String[] shellHistory)
 	{
-		if(command != "exit" || command != "help")
+		stack.push(aCount, lineCount+": "+shellHistory[0]);
+		System.out.println("''"+shellHistory[0]+"'' is an invalid command.");
+	}
+	
+	public void setExitCommand(String commandLine)
+	{
+		if(commandLine.equalsIgnoreCase("exit"))
 		{
-			System.out.println("Wrong Command, you can have some help with -help command!S");
-			return true;
+			System.out.println(this.EXIT);
+			System.exit(0);
 		}
-		return null;
+	}
+	
+	public void setVersionCommand(StringStack stack, int aCount, int lineCount, String commandLine, String[] shellHistory)
+	{
+		stack.push(aCount, lineCount+": "+shellHistory[0]);
+		System.out.println(this.VERSION);
+	}
+	
+	public void setHelpCommand(StringStack stack, int aCount, int lineCount, String commandLine, String[] shellHistory)
+	{
+		stack.push(aCount, lineCount+": "+shellHistory[0]);
+		System.out.println(this.HELP);
+	}
+	
+	public void setTimeCommand(StringStack stack, int aCount, int lineCount, String commandLine, String[] shellHistory)
+	{
+		  stack.push(aCount, lineCount+": "+shellHistory[0]);
+		  String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+		  System.out.println(TIME+timeStamp.toString());	
+	}
+	
+	public void setDateCommand(StringStack stack, int aCount, int lineCount, String commandLine, String[] shellHistory)
+	{
+		  stack.push(aCount, lineCount+": "+shellHistory[0]);
+		  Date date = new Date();
+		  System.out.println(date.toString());
+	}
+	
+	public void setHistoryCommand(StringStack stack, int aCount, int lineCount, String commandLine, String[] shellHistory)
+	{
+		stack.push(aCount, lineCount+": "+shellHistory[0]);
 	}
 
+	public void printHistory(int aCount, StringStack stack, boolean isMoreThan10)
+	{
+		if (isMoreThan10 == false)
+			for(int i=0; i<aCount; i++)
+			{
+				System.out.println("      "+stack.peek(i));
+			}
+		else{
+			for(int i=0; i<10; i++)
+			{
+				System.out.println("      "+stack.peek(i));
+			}
+		}
+	}
+	
 }
--- src/com/project/shell/ShellParser.java
+++ src/com/project/shell/ShellParser.java
@@ -2,18 +2,16 @@ package com.project.shell;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Calendar;
 
 public class ShellParser
 {
-	
+
 	private int lineCount=1, aCount=0;
 	private String commandLine, tag="LF,RD";
-	private String[] lineHistory = new String[100];
-
 	
+	private StringStack stack = new StringStack(10);
+	private CommandList commandList = new CommandList();
+		
 	public void readLine()
 	{
 			
@@ -22,79 +20,136 @@ public class ShellParser
 	InputStreamReader line = new InputStreamReader(System.in);
 	BufferedReader console = new BufferedReader(line);
 	
-	
 	// we break out with <control><C>
 	while (true) {
 		
 	  // read what the user entered
 	  System.out.print(this.tag+"-"+this.lineCount+"> ");
 	  commandLine = console.readLine();
-	  	  
 	  
 	  String[] shellHistory = commandLine.split(" ");
 
-	  
-	  if(this.aCount<=10)
+	  if(this.aCount<10) // we count 10 commands for the history
 	  {
 	  // if the user entered a return, just loop again
 	  if (commandLine.equals(""))
 	  {
 		  continue;
-		} else if(commandLine.equalsIgnoreCase("exit"))  
-		{
+	  } else if(commandLine.equalsIgnoreCase("exit"))  
+	  {
 		  this.lineCount++;
-		  System.out.println("Goodbye");
-		  System.exit(0);
+		  this.commandList.setExitCommand(commandLine);
+		  
 		} else if(commandLine.equalsIgnoreCase("version"))  
 		{
-			lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
+			  commandList.setVersionCommand(stack, aCount, lineCount, commandLine, shellHistory);;
 			  this.lineCount++;
-			  System.out.println("current version: v1.0");
 			  aCount++;
 		} else if(commandLine.equalsIgnoreCase("help"))  
 		{
-			  lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
+			  commandList.setHelpCommand(stack, aCount, lineCount, commandLine, shellHistory);;
 		      this.lineCount++;
-			  System.out.println("list of commands: exit, version, help, time, date, history");
 			  aCount++;
 		}
 		else if(commandLine.equalsIgnoreCase("time"))  
 		{
-			  lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
-			  String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+			  commandList.setTimeCommand(stack, aCount, lineCount, commandLine, shellHistory);;
 		      this.lineCount++;
-			  System.out.println("The current time is: "+timeStamp.toString());
 			  aCount++;
 		}else if(commandLine.equalsIgnoreCase("date"))  
 		{
-			  lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
-			  Date date = new Date();
-			  System.out.println(date.toString());
+			  commandList.setDateCommand(stack, aCount, lineCount, commandLine, shellHistory);;
 		      this.lineCount++;
 			  aCount++;
 		}else if (commandLine.equalsIgnoreCase("history"))
 		{
-			lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
-  			for(int i=0; i<this.aCount; i++)
-  			{
-  			System.out.println(lineHistory[i]);
-  			}
+			commandList.setHistoryCommand(stack, aCount, lineCount, commandLine, shellHistory);;
 			this.lineCount++;
 			aCount++;
+			commandList.printHistory(this.aCount, this.stack, false);
+			
 		}else if (commandLine.equals("exit")==false || commandLine.equals("version")==false  || commandLine.equals("time")==false || commandLine.equals("date")==false || commandLine.equals("history")==false)
 		{
-			lineHistory[aCount]= "History line: "+this.tag+(this.lineCount-1)+" "+shellHistory[0];
-			System.out.println("''"+shellHistory[0]+"'' is an invalid command.");
+			commandList.checkCommand(stack, aCount, lineCount, commandLine, shellHistory);
 			this.lineCount++;
 			aCount++;
-		
 		}
-	  }else{
-		  this.aCount=0;
+	  }else{ //else if the count of commands is more than 10 than...
+
+		   
+//		   if(commandLine.equalsIgnoreCase("version"))  
+//			{
+//				  this.stack.push(9, this.lineCount+": "+shellHistory[0]);
+//				  this.lineCount++;
+//				  System.out.println("Leonardo Farinha & Rohan Dayananda's CLI, Version 1.0");
+//				  aCount++;
+//			}else if (commandLine.equalsIgnoreCase("history"))
+//			{
+//				stack.pop();
+//				this.stack.push(9, this.lineCount+": "+shellHistory[0]);
+//				this.lineCount++;
+//				aCount++;
+//							
+//				for(int i=0; i<10; i++)
+//				{
+//					System.out.println("          ---more than 10--    "+this.stack.peek(i));
+//				}
+//
+//			}
+			
+		  if (commandLine.equals(""))
+		  {
+			  continue;
+		  } else if(commandLine.equalsIgnoreCase("exit"))  
+		  {
+			  this.lineCount++;
+			  this.commandList.setExitCommand(commandLine);
+			  
+			} else if(commandLine.equalsIgnoreCase("version"))  
+			{
+				  stack.pop();
+				  commandList.setVersionCommand(stack, 9, lineCount, commandLine, shellHistory);;
+				  this.lineCount++;
+				  aCount++;
+			} else if(commandLine.equalsIgnoreCase("help"))  
+			{
+				  stack.pop();
+				  commandList.setHelpCommand(stack, 9, lineCount, commandLine, shellHistory);;
+			      this.lineCount++;
+				  aCount++;
+			}
+			else if(commandLine.equalsIgnoreCase("time"))  
+			{
+				  stack.pop();
+				  commandList.setTimeCommand(stack, 9, lineCount, commandLine, shellHistory);;
+			      this.lineCount++;
+				  aCount++;
+			}else if(commandLine.equalsIgnoreCase("date"))  
+			{
+				  stack.pop();
+				  commandList.setDateCommand(stack, 9, lineCount, commandLine, shellHistory);;
+			      this.lineCount++;
+				  aCount++;
+			}else if (commandLine.equalsIgnoreCase("history"))
+			{
+				stack.pop();
+				commandList.setHistoryCommand(stack, 9, lineCount, commandLine, shellHistory);;
+				this.lineCount++;
+				aCount++;
+				commandList.printHistory(9, this.stack, true);
+				
+			}else if (commandLine.equals("exit")==false || commandLine.equals("version")==false  || commandLine.equals("time")==false || commandLine.equals("date")==false || commandLine.equals("history")==false)
+			{
+			   // stack.pop();
+				commandList.checkCommand(stack, 9, lineCount, commandLine, shellHistory);
+				this.lineCount++;
+				aCount++;
+			}
+		  
 	  }
 	}
     } catch (Exception e) {
-        e.printStackTrace();
+			e.printStackTrace();
       }
 	}
 	
